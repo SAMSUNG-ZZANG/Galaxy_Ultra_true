20ac0a6c923d9695dbc0661053eab56ea0c503a1
jinsil <wryt9224@gmail.com>
2022-06-10 03:06:12 +0900

    [FEAT] #5 온보딩 구현

--- app/src/main/java/com/example/sopt_main/ui/fragment/onboarding/SampleFragment3.kt
+++ app/src/main/java/com/example/sopt_main/ui/fragment/onboarding/SampleFragment3.kt
@@ -1,60 +1,51 @@
 package com.example.sopt_main.ui.fragment.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.bumptech.glide.Glide
 import com.example.sopt_main.R
+import com.example.sopt_main.adapter.FollowerAdapter
+import com.example.sopt_main.databinding.FragmentFollowerBinding
+import com.example.sopt_main.databinding.FragmentSample3Binding
+import com.example.sopt_main.ui.activity.SettingActivity
+import com.example.sopt_main.ui.activity.SignInActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SampleFragment3.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SampleFragment3 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentSample3Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sample3, container, false)
+        _binding = FragmentSample3Binding.inflate(layoutInflater,container,false)
+
+
+        initImage()
+        btnClickEvent()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SampleFragment3.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SampleFragment3().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private  fun initImage(){
+        Glide.with(this)
+            .load(R.drawable.selfie_true)
+            .circleCrop()
+            .into(binding.ivProfile)
     }
+
+    private fun btnClickEvent(){
+        _binding?.btnNext?.setOnClickListener{
+            val intent = Intent(activity, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
 }
