a23673421e85900006f2186e37d50c4051317ceb
jinsilWoo <wryt9224@gmail.com>
2022-07-02 00:59:06 +0900

    [MODIFY] 코드리뷰 적용

--- app/src/main/java/com/example/sopt_main/server/response/ResponseFollowerInfo.kt
+++ app/src/main/java/com/example/sopt_main/server/response/ResponseFollowerInfo.kt
@@ -1,8 +1,9 @@
 package com.example.sopt_main.server.response
+import kotlinx.serialization.Serializable
 
+
+@Serializable
 data class ResponseFollowerInfo(
-
     val login: String,
     val avatar_url: String
-
 )
