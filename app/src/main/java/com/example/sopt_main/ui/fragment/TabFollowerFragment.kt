2c9649331dfd297064b88a8e0bec41e8d98295e5
jinsilWoo <wryt9224@gmail.com>
2022-08-28 23:58:33 +0900

    modify

--- app/src/main/java/com/example/sopt_main/ui/fragment/TabFollowerFragment.kt
+++ app/src/main/java/com/example/sopt_main/ui/fragment/TabFollowerFragment.kt
@@ -1,10 +1,7 @@
 package com.example.sopt_main.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.sopt_main.R
 import com.example.sopt_main.base.BaseFragment
 import com.example.sopt_main.databinding.FragmentTabFollowerBinding
