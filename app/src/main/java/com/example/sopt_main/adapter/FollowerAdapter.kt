dbb33e70d269485677f0ab143ef8170d1ebfeab7
jinsilWoo <wryt9224@gmail.com>
2022-09-05 22:59:38 +0900

    [FEAT]#10-RecyclerView item 클릭 시 이동

--- app/src/main/java/com/example/sopt_main/adapter/FollowerAdapter.kt
+++ app/src/main/java/com/example/sopt_main/adapter/FollowerAdapter.kt
@@ -4,33 +4,48 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.sopt_main.server.response.ResponseFollowerInfo
 import com.example.sopt_main.databinding.FollowerListBinding
+import com.example.sopt_main.server.response.ResponseFollowerInfo
 
-class FollowerAdapter : RecyclerView.Adapter<FollowerAdapter.FollowerViewHolder>(){
+class FollowerAdapter : RecyclerView.Adapter<FollowerAdapter.FollowerViewHolder>() {
+
     var followerList = mutableListOf<ResponseFollowerInfo>()
+    private var itemClickListener: OnItemClickListener? = null
 
-    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int): FollowerViewHolder {
-        val binding = FollowerListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return  FollowerViewHolder(binding)
+
+    interface OnItemClickListener {
+        fun onClick(data: ResponseFollowerInfo)
+    }
+
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+        val binding =
+            FollowerListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FollowerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
         holder.onBind(followerList[position])
+        holder.itemView.setOnClickListener {
+            itemClickListener?.onClick(followerList[position])
+        }
+
     }
 
     override fun getItemCount(): Int = followerList.size
 
     class FollowerViewHolder(
-            private val binding:FollowerListBinding
-    ) : RecyclerView.ViewHolder(binding.root){
-        fun onBind(data: ResponseFollowerInfo){
+        private val binding: FollowerListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ResponseFollowerInfo) {
             binding.followerName.text = data.login
             Glide.with(binding.root)
                 .load(data.avatar_url)
                 .circleCrop()
                 .into(binding.followerProfile)
-
-            }
+        }
     }
 }
--- app/src/main/java/com/example/sopt_main/ui/fragment/FollowerFragment.kt
+++ app/src/main/java/com/example/sopt_main/ui/fragment/FollowerFragment.kt
@@ -1,41 +1,58 @@
 package com.example.sopt_main.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.sopt_main.R
 import com.example.sopt_main.adapter.FollowerAdapter
 import com.example.sopt_main.base.BaseFragment
 import com.example.sopt_main.databinding.FragmentFollowerBinding
+import com.example.sopt_main.server.response.ResponseFollowerInfo
+import com.example.sopt_main.ui.activity.DetailActivity
 import com.example.sopt_main.ui.viewmodel.FollowerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FollowerFragment : BaseFragment<FragmentFollowerBinding>(R.layout.fragment_follower) {
-    private lateinit var followerAdapter: FollowerAdapter
+    private var followerAdapter: FollowerAdapter? = FollowerAdapter()
     private val viewModel: FollowerViewModel by viewModels()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        followerAdapter = FollowerAdapter()
+        binding.rvFollower.adapter = followerAdapter
+        itemClickEvent()
         viewModel.fetchFollowers()
         followerUserNetwork()
     }
 
 
-    private fun followerUserNetwork(){
+    private fun followerUserNetwork() {
         viewModel.followerList.observe(viewLifecycleOwner) {
-            followerAdapter = FollowerAdapter()
-            binding.rvFollower.adapter = followerAdapter
             if (it != null) {
-                followerAdapter.followerList = it.toMutableList()
+                followerAdapter?.followerList = it.toMutableList()
             }
-            followerAdapter.notifyDataSetChanged()
+            followerAdapter?.notifyDataSetChanged()
         }
     }
 
+    private fun itemClickEvent() {
+            followerAdapter?.setItemClickListener(object : FollowerAdapter.OnItemClickListener {
+                override fun onClick(data:ResponseFollowerInfo) {
+                    // 클릭 시 이벤트 작성
+                    val image = data.avatar_url
+                    val login = data.login
+                    val intent = Intent(context, DetailActivity::class.java).apply {
+                        putExtra("image", image)
+                        putExtra("login", login)
+                    }
+                    startActivity(intent)
+                }
+            })
+
+
+
+    }
 }
