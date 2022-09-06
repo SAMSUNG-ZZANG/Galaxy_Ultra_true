fbf5fbd00b254254498dd4fc147d2e095a0611da
jinsilWoo <wryt9224@gmail.com>
2022-09-06 21:57:57 +0900

    [FEAT]#10- ItemTouchHelper 구현

---
+++ app/src/main/java/com/example/sopt_main/MyItemTouchHelperCallBack.kt
@@ -0,0 +1,36 @@
+package com.example.sopt_main
+
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+class MyItemTouchHelperCallBack(val onItemTouchHelper: ItemTouchHelperListener) :
+    ItemTouchHelper.Callback() {
+
+    interface ItemTouchHelperListener {
+        fun onItemMoved(fromPosition: Int, toPosition: Int): Boolean
+        fun onItemSwiped(position: Int)
+    }
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+        val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        return makeMovementFlags(dragFlags, 0)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return onItemTouchHelper.onItemMoved(viewHolder.adapterPosition, target.adapterPosition)
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        onItemTouchHelper.onItemSwiped(viewHolder.adapterPosition)
+    }
+
+
+}
